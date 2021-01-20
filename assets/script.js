ffa4ceb59e147dd6dee6d1a15b222aa31d531d2e
Alexey Kuzmis <alexeykuzmis@Alexeys-MacBook-Pro.local>
2021-01-20 20:34:00 +0000

    js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 const filterRef= document.getElementById('filter');
 const clearRef = document.getElementById('clear');
-// const crossRef = filterRef.children[0].children[0].children[1];
+// const crossRef = document.getElementById('filter').children[0].children[0].children[1]
 const buttonRef = document.querySelector('.button-wrapper')
 const buttonRole = document.querySelector('.button');
+const rolesRef = document.getElementById('rolesRef');
 
-console.log(filterRef.children[0].children[0])
-
+// console.log(crossRef)
 
 function filterHandler(){
     filterRef.style.display=('flex');
@@ -13,16 +13,8 @@ function filterHandler(){
 }
 
 
-// function roleButtonRefHandler(){
-//     addEventListener('click', (event) => {
-//         console.log(event.target.parentElement)
-//         if(event.target.parentElement.className = 'cross'){
-//             event.target.parentElement.parentElement.style.display = 'none';
-//         }
-//     })
-// }
 
-// roleButtonRefHandler()
+
 
 
 function newElement(el) {
@@ -39,17 +31,41 @@ function newElement(el) {
     console.log(el)
     divRef.appendChild(divCross);
     document.getElementById("rolesRef").appendChild(divRef);
-   
+    // roleButtonRefHandler()
 }
-  
+
+
+
 function buttonRoleHandler(){
     buttons = document.querySelectorAll('.button')
     buttons.forEach(button => button.addEventListener('click', (e)=>{
         click = e.target;
+        console.log(click)
         newElement(click.innerHTML)
         filterHandler()
-    }))
         
-
+    }))
 }
 buttonRoleHandler()
+
+function removeFilter(){
+    filterButtons = filterRef.children[0].children[0]
+    console.log(filterButtons)
+
+        filterRef.children[0].children[0].style.display = 'none'
+    
+}
+clearRef.addEventListener('click', removeFilter)
+
+// function roleButtonRefHandler(){
+//     filterRef.children[0].children[0].children[1].addEventListener('click', (event) => {
+//         console.log(event.target)
+//         roleToRemove = event.target.parentElement
+//         buttonWrapper = filterRef.children[0].children[0]
+//         console.log(roleToRemove)
+//         for(i=0;i <roleToRemove.length;i++){
+//             roleToRemove[i].style.display = 'none';
+//             console.log(roleToRemove[i])
+//         }
+//     })
+// }
